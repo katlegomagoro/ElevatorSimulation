3c3070a4b981463d41cd43fa0f9b7b6e514f7031
Katlego Magoro <katlego.magoro@expleogroup.com>
2025-05-13 22:55:53 +0200

    added PassengerRequestHandler for boarding and unloading logic for servese

--- ElevatorSimulation/Services/DispatcherService.cs
+++ ElevatorSimulation/Services/DispatcherService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevatorSimulation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace ElevatorSimulation.Services
 {
-    internal class DispatcherService
+    public class DispatcherService
     {
+        private readonly List<Elevator> _elevators;
+
+        public DispatcherService(List<Elevator> elevators)
+        {
+            _elevators = elevators;
+        }
+
+        public Elevator? AssignBestElevator(int requestedFloor)
+        {
+            // Filter available elevators
+            var availableElevators = _elevators.Where(e => e.IsAvailable).ToList();
+
+            if (!availableElevators.Any())
+                return null;
+
+            // Choose the closest elevator 
+            return availableElevators
+                .OrderBy(e => Math.Abs(e.CurrentFloor - requestedFloor))
+                .First();
+        }
     }
 }
