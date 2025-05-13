fb876a7ec0b245463bef431f15949b4e62edb4d9
Katlego Magoro <katlego.magoro@expleogroup.com>
2025-05-13 21:53:39 +0200

    Cleaned tracked build artifacts and added .gitignore

---
+++ ElevatorSimulation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ElevatorSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
